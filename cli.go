1bee093425d16f5129ceb5a421dc1a813cda5bac
Brandon Hilkert <brandonhilkert@gmail.com>
2015-10-01 17:36:57 -0400

    Setup queues

--- cli.go
+++ cli.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"mime"
 	"os"
@@ -9,10 +10,10 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/s3"
-	"github.com/rakyll/pb"
 )
 
 const (
@@ -22,13 +23,103 @@ const (
 )
 
 var (
-	bucket    = flag.String("b", "assets.pipelinedeals.com", "AWS S3 bucket to upload assets to")
-	directory = flag.String("d", "./public", "Local assets directory to upload to S3")
-	key       = flag.String("k", "", "Key to preface to asset")
-	help      = flag.Bool("help", false, "You're looking at it")
-	h         = flag.Bool("h", false, "You're looking at it")
-	version   = flag.Bool("v", false, "Show version")
+	bucket       = flag.String("b", "assets.pipelinedeals.com", "AWS S3 bucket to upload assets to")
+	directory    = flag.String("d", "./public", "Local assets directory to upload to S3")
+	key          = flag.String("k", "", "Key to preface to asset")
+	maxWorkers   = flag.Int("w", 4, "Max number of workers to start")
+	maxQueueSize = flag.Int("q", 100, "Max size of upload queue")
+	help         = flag.Bool("help", false, "You're looking at it")
+	h            = flag.Bool("h", false, "You're looking at it")
+	version      = flag.Bool("v", false, "Show version")
 )
+
+type Upload struct {
+	Path      string
+	Dir       string
+	Bucket    string
+	PrefixKey string
+	WaitGroup *sync.WaitGroup
+}
+
+func (u *Upload) RelativePath() string {
+	relativePath := strings.TrimPrefix(u.Path, u.Dir+"/")
+
+	if u.PrefixKey != "" {
+		relativePath = "/" + u.PrefixKey + "/" + relativePath
+	}
+	return relativePath
+}
+
+func (u *Upload) FileType() string {
+	ext := filepath.Ext(u.Path)
+	fileType := mime.TypeByExtension(ext)
+	return fileType
+}
+
+func (u *Upload) Put() {
+	auth, err := aws.EnvAuth()
+
+	if err != nil {
+		panic(err)
+	}
+
+	client := s3.New(auth, aws.USEast)
+	b := client.Bucket(u.Bucket)
+
+	file, err := os.Open(u.Path)
+	if err != nil {
+		panic(err)
+	}
+
+	stat, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	headers := map[string][]string{
+		"Content-Length": {strconv.FormatInt(stat.Size(), 10)},
+		"Content-Type":   {u.FileType()},
+		"Cache-Control":  {"max-age=31104000"},
+	}
+
+	relativePath := u.RelativePath()
+
+	err = b.PutReaderHeader(relativePath, file, stat.Size(), headers, s3.ACL("public-read"))
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+type Worker struct {
+	id          int
+	uploadQueue chan *Upload
+}
+
+func NewWorker(id int, uploadQueue chan *Upload) Worker {
+	return Worker{
+		id:          id,
+		uploadQueue: uploadQueue,
+	}
+}
+
+func (w Worker) start() {
+	go func() {
+		for {
+			select {
+			case upload := <-w.uploadQueue:
+				// Dispatcher has added a upload to my upload.
+				fmt.Printf("worker%d started: uploading %s to %s/%s/%s\n", w.id, upload.Path, upload.Bucket, upload.PrefixKey, upload.Path)
+				upload.Put()
+
+				upload.WaitGroup.Done()
+
+				fmt.Printf("worker%d finished uploading %s!\n", w.id, upload.Path)
+			}
+		}
+	}()
+}
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -57,14 +148,30 @@ func main() {
 		panic(err)
 	}
 
-	count := len(fileList)
-	bar := pb.StartNew(count)
+	uploadQueue := make(chan *Upload, *maxQueueSize)
+	var wg sync.WaitGroup
+
+	for i := 0; i < *maxWorkers; i++ {
+		worker := NewWorker(i, uploadQueue)
+		worker.start()
+	}
 
 	for _, path := range fileList {
-		UploadFile(path, *directory, *bucket, *key)
-		bar.Increment()
+		upload := &Upload{
+			Path:      path,
+			Dir:       *directory,
+			Bucket:    *bucket,
+			PrefixKey: *key,
+			WaitGroup: &wg,
+		}
+
+		wg.Add(1)
+		go func() {
+			uploadQueue <- upload
+		}()
 	}
-	bar.FinishPrint("All done!")
+
+	wg.Wait()
 }
 
 func GetFileList(dir string) (fileList []string, err error) {
@@ -78,52 +185,12 @@ func GetFileList(dir string) (fileList []string, err error) {
 	return fileList, err
 }
 
-func UploadFile(path string, dir string, bucketName string, prefixKey string) {
-	auth, err := aws.EnvAuth()
-
-	if err != nil {
-		panic(err)
-	}
-
-	client := s3.New(auth, aws.USEast)
-	b := client.Bucket(bucketName)
-
-	file, err := os.Open(path)
-	if err != nil {
-		panic(err)
-	}
-
-	stat, err := file.Stat()
-	if err != nil {
-		panic(err)
-	}
-
-	ext := filepath.Ext(path)
-	fileType := mime.TypeByExtension(ext)
-
-	headers := map[string][]string{
-		"Content-Length": {strconv.FormatInt(stat.Size(), 10)},
-		"Content-Type":   {fileType},
-		"Cache-Control":  {"max-age=31104000"},
-	}
-
-	relativePath := strings.TrimPrefix(path, dir+"/")
-
-	if prefixKey != "" {
-		relativePath = "/" + prefixKey + "/" + relativePath
-	}
-
-	err = b.PutReaderHeader(relativePath, file, stat.Size(), headers, s3.ACL("public-read"))
-
-	if err != nil {
-		panic(err)
-	}
-}
-
 func printHelp() {
 	log.Println("-b\t\tAWS S3 target bucket, default: assets.pipelinedeals.com")
 	log.Println("-d\t\tLocal assets directory to upload to S3, default: ./public")
 	log.Println("-k\t\tKey to preface the asset with, default: ''")
+	log.Println("-w\t\tMax number of workers to start, default: 4")
+	log.Println("-q\t\tKey Max size of upload queue, default: 100")
 	log.Println("-v\t\tShow version and license information")
 	log.Println("-h\t\tThis help screen")
 }
